a9e2e0f7442808507bfe405e67ec9a9d6ec934fe
Alex <mister.protasov@gmail.com>
2023-08-29 15:08:30 +0200

    [MASTER]: minor fixes

--- src/widgets/loading/ui/index.tsx
+++ src/widgets/loading/ui/index.tsx
@@ -2,7 +2,11 @@ import Image from 'next/image';
 import { LoadingGif } from '@shared/assets';
 import styles from './styles.module.scss';
 
-export const LoaderComponent = () => {
+export const LoaderComponent = () => {  
+
+
+
+  
   return (
     <div className={styles.loading}>
       <Image src={LoadingGif} alt="loading" />
